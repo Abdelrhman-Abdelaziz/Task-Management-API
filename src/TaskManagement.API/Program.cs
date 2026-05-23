6582287c3be82731271367a8b404853cb5cc5f85
unknown <Abdelrhmanhabdelaziz@gmail.com>
2026-05-23 14:31:43 +0300

    feat(api): configure Swagger UI with JWT bearer authentication

--- src/TaskManagement.API/Program.cs
+++ src/TaskManagement.API/Program.cs
@@ -6,12 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApiVersioningConfiguration();
+builder.Services.AddSwaggerConfiguration();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseExceptionHandling();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwaggerConfiguration();
+}
 
 app.UseHttpsRedirection();
 
